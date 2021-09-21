132d9a736e72ea699401407e157931a28418712c
GaGiiiii <dragoslav.gagi8@gmail.com>
2021-09-22 01:07:37 +0200

    Almost over

--- resources/views/components/owl-row.blade.php
+++ resources/views/components/owl-row.blade.php
@@ -1,5 +1,5 @@
 <div class="row mt-5">
-    <h1 style="margin-bottom: 20px; font-size: 1.8rem">{{ $title }}</h1>
+    <h1 style="margin-bottom: 20px; font-size: 1.8rem; font-weight: 900;">{{ $title }} <hr style="margin-bottom: 20px; margin-top: 10px;"></h1>
     <div class="owl-carousel owl-theme {{ $type }}">
         @foreach ($products as $product)
             <a href="{{ url('products/' . $product->id) }}">
